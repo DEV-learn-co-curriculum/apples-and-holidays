e546bc58d861ded5afd0d6b84eb2cf68f179f5f1
Daniel Chang <danielchangNYC@users.noreply.github.com>
2014-09-23 09:53:38 -0400

    Update apple_spec.rb

--- spec/apple_spec.rb
+++ spec/apple_spec.rb
@@ -5,7 +5,17 @@ describe "Picking Apples" do
   
   describe "#apple_picker_with_select" do
     it "should return an array of apples" do
-      expect(true).to eq true
+      expect(apple_picker_with_select(fruits)).to_not include("orange")
+    end
+  end
+  
+  describe "#apple_picker_with_collect" do
+    it "should return an array of apples" do
+      expect(apple_picker_with_collect(fruits)).to_not include("orange")
+    end
+    
+    it "should return an array without nil elements" do
+      expect(apple_picker_with_collect(fruits)).to_not include(nil)
     end
   end
 end
